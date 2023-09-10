26a242512c305e83b1a6504e6dd117436da5bfc8
aerofotosintez <aerofotosintez@gmail.com>
2023-09-10 20:32:04 +0300

    Seminar1_Task4 автоформат

--- Seminar1/Task4.py
+++ Seminar1/Task4.py
@@ -9,7 +9,7 @@
 # Output: YES
 
 year = int(input('введите год: '))
-if (year%4 == 0 and year%100 != 0) or (year%400 == 0):
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print('yes')
 else:
     print('no')

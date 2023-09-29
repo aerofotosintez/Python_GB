b4cc38350ad83e20aff79eecfabe41cef8151edc
aerofotosintez <aerofotosintez@gmail.com>
2023-09-29 20:01:03 +0300

    Seminar8 Task49

--- Seminar8/Task49.py
+++ Seminar8/Task49.py
@@ -22,48 +22,87 @@ def main():
     data = {}
     if os.path.exists(file_name):
         with open(file_name) as f:
-            data = {name: num for name, num in line.split('\t') for line f.readlines() if line in f.readlies() if line}
+            for line in f.readlines():
+                if line:
+                    name, num = line.split('\t')
+                    data[name] = num
+            # data = {name: num for name, num in line.split(
+            #     '\t') for line in f.readlines() if line}
     else:
         with open(file_name, 'w') as f:
             pass
-while True:
+
     while True:
-        print('1.Ввести данные')
-        print('2.Поиск')
-        print('3.Выход')
-        user_choice = input('Введите выбор (1-3): ')
-        if user_choice not in ['1', '2', '3']:
+        while True:
+            print('1.Ввести данные')
+            print('2.Поиск')
+            print('3.Выход')
+            user_choice = input('Введите выбор (1-3): ')
+            if user_choice not in ['1', '2', '3']:
+                print('Ошибка')
+            else:
+                break
+        match user_choice:
+            case '1':
+                data = data_input(data)
+            case '2':
+                data_search(data)
+            case '3':
+                print('Досвидание')
+                if not data:
+                    return
+                with open(file_name, 'w') as f:
+                    for name in data:
+                        print(f'{name}\t{data[name]}', file=f)
+                return
+
+
+def data_input(data):
+    name = input('Введите ФИО: ')
+    if name and len(name.split()) == 3:
+        num = input('Введите телефон: ')
+        if num and num.isdigit():
+            data[name.replace('\t', ' ')] = num
+            return data
+    print('Неверный ввод ')
+    return data
+
+def data_search(data):
+    user_input = input('Введите данные для поиска: ')
+    while True:
+        print('1.Найти фамилию ')
+        print('2.Найти имя ')
+        print('3.Найти отчество ')
+        print('4.Найти номер тлф ')
+        print('5.Вернуться в меню ')
+        user_choice = input('Введите выбор (1-5): ')
+        if user_choice not in ['1', '2', '3', '4', '5']:
             print('Ошибка')
         else:
             break
     match user_choice:
         case '1':
-            data = data_input(data)
+            for key in data:
+                name1, name2, name3 = key.split()
+                if name1 == user_input:
+                    print(f"{key} {data[key]}")
         case '2':
-            data_search(data)
+            for key in data:
+                name1, name2, name3 = key.split()
+                if name2 == user_input:
+                    print(f'{key} {data[key]}')
         case '3':
-            print('Досвидание')
-            with open(file_name, 'w') as f:
-                for name in data:
-                    print(f'{name}\t{data[name]}')
+            for key in data:
+                name1, name2, name3 = key.split()
+                if name3 == user_input:
+                    print(f'{key} {data[key]}')
+        case '4':
+            for key, value in data.items():
+                if value == user_input():
+                    print(f'{key} {data[key]}')
+        case '5':
+            print('Выход ')
             return
         
-
-def data_input(data):
-    name = input('Введите ФИО: ')
-    if name and name.isalpha() and len(name.split()) == 3:
-        num = input('Введите телефон: ')
-        if num and num.isdigit():
-            data[name] = num
-        else:
-            return data
-    else:
-        print('Неверный ввод: ')
-
-
-def data_search():
-    
-
-
-def print_d(data):
-
+if __name__ == '__main__':
+    main()
